d29121eadb192b8ccd7f14017c214d5f9e556802
craig <craigsc333@gmail.com>
2012-04-20 19:46:23 -0400

    Initial commit of android controller.

---
+++ HesController/src/com/hes/hescontroller/HesControllerActivity.java
@@ -0,0 +1,13 @@
+package com.hes.hescontroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HesControllerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

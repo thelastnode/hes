703792ca51b8d5982307f3f1aa99fc8ded932828
Ankit Shankar <ankitshankar@gmail.com>
2012-04-20 20:25:42 -0400

    Bidirectional TCP

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ var server = net.createServer(function(socket) {
   carrier.carry(socket, function(line) {
     console.log('LINE: %s', line);
   });
+
+  setTimeout(function() {
+    var line = JSON.stringify({
+      foo: 5,
+      bar: 'yay',
+    });
+    console.log('SEND: %s', line);
+    line = new Buffer(line).toString('base64');
+    socket.write(line + '\n');
+  }, 1000);
 });
 
 module.exports = server;

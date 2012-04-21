e2e1853f54a831056f07a94cd10544b6ed66b51a
craig <craigsc333@gmail.com>
2012-04-20 22:55:55 -0400

    Controller now detecting touch inputs and sending to server.

--- HesController/src/com/hes/hescontroller/HesControllerActivity.java
+++ HesController/src/com/hes/hescontroller/HesControllerActivity.java
@@ -1,14 +1,28 @@
 package com.hes.hescontroller;
 
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.hes.hescontroller.views.Joysticks;
 
 public class HesControllerActivity extends Activity {
+	private boolean stopped = false;
+	private ArrayList<String> buffer = new ArrayList<String>();
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,6 +30,61 @@ public class HesControllerActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
+        
+        new Thread(new Runnable() {
+        	Socket s = null;
+            public void run() {
+            	try {
+                    s = new Socket("10.0.2.2",4000);
+                    
+                    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+                    while (!stopped) {
+                    	sendNewMoves(bw);
+                    }
+            } catch (UnknownHostException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+            } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+            }
+          }
+        }).start();
+        
         Joysticks joys = (Joysticks) findViewById(R.id.joysticks);
+        joys.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				Log.d("HES", "TOUCH EVENT");
+				float x = event.getX(0);
+				float y = event.getY(0);
+				float x2 = 0, y2 = 0;
+				if (event.getPointerCount() > 1) {
+					x2 = event.getX(1);
+					y2 = event.getY(1);
+				}
+//				if (joys.isInLeft()) {
+//					x = joys.getLeftCoord(x);
+//					y = joys.getRightCoord(y);
+//				}
+//				if (joys.isInRight()) {
+//					x = joys.getLeftCoord(x);
+//					y = joys.getRightCoord(y);
+//				}
+				buffer.add(toCommand(x, y, x2, y2));
+				return false;
+			}
+        });
+    }
+    
+    public String toCommand(float x, float y, float x2, float y2) {
+    	return "{\"left\": {\"x\":" + x + ", \"y\":" + y + "}, \"right\": {\"x\":" + x2 + ", \"y\":" + y2 + "}}";
+    }
+    
+    
+    public void sendNewMoves(BufferedWriter out) throws IOException {
+    	while (!buffer.isEmpty()) {
+    		out.write(Base64.encodeToString(buffer.remove(0).getBytes(), Base64.NO_WRAP) + "\n");
+    		out.flush();
+    	}
     }
 }
--- HesController/src/com/hes/hescontroller/views/Joysticks.java
+++ HesController/src/com/hes/hescontroller/views/Joysticks.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 
 public class Joysticks extends View {
 	protected int radius = 100;
+	protected float width, height;
 	private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 	
 	public Joysticks(Context c, AttributeSet as) {
@@ -23,9 +25,27 @@ public class Joysticks extends View {
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		width = canvas.getWidth();
+		height = canvas.getHeight();
 		paint.setColor(0xFFFF0000);
 		paint.setStyle(Paint.Style.FILL);
-		canvas.drawCircle(100, canvas.getHeight() - radius, radius, paint);
+		canvas.drawCircle(radius, canvas.getHeight() - radius, radius, paint);
 		canvas.drawCircle(canvas.getWidth() - radius, radius, radius, paint);
+	}
+	
+	public float getLX() {
+		return radius;
+	}
+	
+	public float getLY() {
+		return height - radius;
+	}
+	
+	public float getRX() {
+		return width - radius;
+	}
+	
+	public float getRY() {
+		return radius;
 	}
 }
